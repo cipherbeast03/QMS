0dabd2b522e2d184dd1fffb5646b022b0e0bd373
cipherbeast03 <ethand@examroom.ai>
2023-11-27 20:50:36 +0530

    audit sheet updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './Footer/footer';
 import Audit from './Audit/audit';
 import AuditReview from './ad-review/review';
 import Dashboard from './Dashboard/dashboard';
+import Monitorpage from './Audit-sheet/first';
 import './style.css';
 
 export default function App() {
@@ -15,6 +16,8 @@ export default function App() {
         <Route exact path="/" element={<Dashboard />} />
         <Route exact path="/audit" element={<Audit />} />
         <Route exact path="/audit-review" element={<AuditReview />} />
+        <Route exact path="/first" element={<AuditReview />} />
+        <Route exact path="/first-page" element={<Monitorpage />} />
       </Routes>
       <Footer />
     </div>
--- src/Audit-sheet/first.js
+++ src/Audit-sheet/first.js
@@ -6,7 +6,22 @@ const Monitorpage = () => {
   return (
     <div class="container">
       <div class="left-div"></div>
-      <div class="right-div"></div>
+      <div class="right-div">
+      <div className="card-container">
+        <div className="card">
+          <h2>section 1</h2>
+          <p>This is the content of section 1</p>
+        </div>
+        <div className="card">
+          <h2>section 2</h2>
+          <p>This is the content of section 2</p>
+        </div>
+        <div className="card">
+          <h2>section 3</h2>
+          <p>This is the content of section 3</p>
+        </div>
+        </div>
+      </div>
     </div>
   );
 };
